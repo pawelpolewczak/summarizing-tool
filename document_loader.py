1e1cc3515268429141566d0a3884a7fa360e11bf
Pawel Polewczak <pawel@polewczak.net>
2024-01-13 11:02:25 +0100

    Update main files

--- document_loader.py
+++ document_loader.py
@@ -1,13 +1,8 @@
-
 class DocumentLoader:
     def __init__(self, file_path):
-        self.document = self.__load_document(file_path)
+        self.file_path = file_path
 
-    # Create a method that takes file path as parameter and returns the file content
-    def __load_document(self, file_path):
-        with open(file_path, 'r') as file:
+    def load_document(self):
+        with open(self.file_path, 'r') as file:
             document = file.read()
         return document
-
-
-
